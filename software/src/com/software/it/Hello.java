e79a26538f0cd41c277309e0601025515e133d4a
Chan3562 <Chan3562@CSL0705.global.chainsys.com>
2024-04-12 15:10:37 +0530

    num

--- software/src/com/software/it/Hello.java
+++ software/src/com/software/it/Hello.java
@@ -5,8 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world:");
-		int c;
-		System.out.println("helo");
+		int c=10;
+		System.out.println("helo"+c);
 	}
 
 }
