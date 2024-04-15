850c9d9b222d645e297d675b423f81e603edd495
Chan3562 <Chan3562@CSL0705.global.chainsys.com>
2024-04-15 16:59:37 +0530

    input and output

---
+++ Codingchallenge/src/com/hacker/rank/InOut.java
@@ -0,0 +1,20 @@
+package com.hacker.rank;
+import java.util.*;
+public class InOut {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d=scan.nextDouble();
+        String s=scan.next();
+        
+               // Write your code here.
+        
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+
+	}
+
+}
